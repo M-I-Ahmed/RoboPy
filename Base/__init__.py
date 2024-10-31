2e369903c4c22d94d4a71f3038dc104d4dbbae4d
Mohammed Ismael Ahmed <egyma1@nottingham.ac.uk>
2024-10-31 14:37:19 +0000

    Initial abstract methods and properties

---
+++ Base/__init__.py
@@ -0,0 +1,5 @@
+### This file imports the robot definition from the abstract robot class allowing it to be accessed in a clear and controlled manner. ###
+
+from .robot import Robot
+
+__all__ = ["Robot"]
