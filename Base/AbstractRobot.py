0bede8cedc7fc6a8ba36924dde2590f19367ca6c
Mohammed Ismael Ahmed <egyma1@nottingham.ac.uk>
2024-11-01 14:13:15 +0000

    Added I/O functions

--- Base/AbstractRobot.py
+++ Base/AbstractRobot.py
@@ -52,6 +52,60 @@ class Robot(ABC):
         raise NotImplementedError("This method should be implemented by any subclasses")
 
     # Program Control # 
+    def start_program(self, program_name: str = None,) -> tuple[Literal[0,1],str]:
+        raise NotImplementedError("This method should be implemented by any subclasses")
+
+    def stop_program(self, program_name: str = None,) -> tuple[Literal[0,1],str]:
+        raise NotImplementedError("This method should be implemented by any subclasses")
+
+    def is_program_running(self)-> tuple[Literal[0,1],str]:
+        raise NotImplementedError("This method should be implemented by any subclasses")
+
+    # Get I/O states # 
+    def get_Din(self, digital_input_index: int, value: bool )-> tuple[Literal[0,1],str]:
+        raise NotImplementedError("This method should be implemented by any subclasses")
+
+    def get_Dout(self, digital_output_index: int, value: bool )-> tuple[Literal[0,1],str]:
+        raise NotImplementedError("This method should be implemented by any subclasses")
+
+    def get_Ain(self, analog_input_index: int, value: bool )-> tuple[Literal[0,1],str]:
+        raise NotImplementedError("This method should be implemented by any subclasses")
+    
+    def get_Aout(self, analog_output_index: int, value: bool )-> tuple[Literal[0,1],str]:
+        raise NotImplementedError("This method should be implemented by any subclasses")
+
+    def get_Gin(self, group_input_index: int, value: bool )-> tuple[Literal[0,1],str]:
+        raise NotImplementedError("This method should be implemented by any subclasses")
+
+    def get_Gout(self, group_output_index: int, value: bool )-> tuple[Literal[0,1],str]:
+        raise NotImplementedError("This method should be implemented by any subclasses")
+
+    # Set I/O states #
+    def set_Din(self, digital_input_index: int, value: bool )-> tuple[Literal[0,1],str]:
+        raise NotImplementedError("This method should be implemented by any subclasses")
+
+    def set_Dout(self, digital_output_index: int, value: bool )-> tuple[Literal[0,1],str]:
+        raise NotImplementedError("This method should be implemented by any subclasses")
+
+    def set_Ain(self, analog_input_index: int, value: bool )-> tuple[Literal[0,1],str]:
+        raise NotImplementedError("This method should be implemented by any subclasses")
+    
+    def set_Aout(self, analog_output_index: int, value: bool )-> tuple[Literal[0,1],str]:
+        raise NotImplementedError("This method should be implemented by any subclasses")
+
+    def set_Gin(self, group_input_index: int, value: bool )-> tuple[Literal[0,1],str]:
+        raise NotImplementedError("This method should be implemented by any subclasses")
+
+    def set_Gout(self, group_output_index: int, value: bool )-> tuple[Literal[0,1],str]:
+        raise NotImplementedError("This method should be implemented by any subclasses")
+
+    # Reset methods
+
+
+
+    # Return to home
+    # Clear program
+
 
 
 
