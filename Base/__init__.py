7e891bfce06f3177a4d70793c01bd31592375bc7
Mohammed Ismael Ahmed <egyma1@nottingham.ac.uk>
2024-11-05 15:02:04 +0000

    Removed ABB code for testing purposes with Fanuc for now

--- Base/__init__.py
+++ Base/__init__.py
@@ -1,5 +1,5 @@
 ### This file imports the robot definition from the abstract robot class allowing it to be accessed in a clear and controlled manner. ###
 
-from .robot import Robot
+from .AbstractRobot import Robot
 
 __all__ = ["Robot"]
